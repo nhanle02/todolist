4b4236478de77340ef9956d77e3c0ffdbad2f4c2
nhanle02 <nhanle.020902@gmail.com>
2023-11-06 20:07:54 +0700

    UI and build react-redux

---
+++ src/components/Broadcast.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Box from './Box';
+
+function Broadcast() {
+  return ( 
+    <>
+      <div className="broad">
+        <div className="broad__item">
+          <div className="broad__item__head">To do</div>
+          <div className="broad__item__content">
+            <Box />
+          </div>
+        </div>
+        <div className="broad__item">
+          <div className="broad__item__head">To do</div>
+          <div className="broad__item__content">
+            <Box />
+          </div>
+        </div>
+        <div className="broad__item">
+          <div className="broad__item__head">To do</div>
+          <div className="broad__item__content">
+            <Box />
+          </div>
+        </div>
+        <div className="broad__item">
+          <div className="broad__item__head">To do</div>
+          <div className="broad__item__content">
+            <Box />
+          </div>
+        </div>
+        <div className="broad__item">
+          <div className="broad__item__head">To do</div>
+          <div className="broad__item__content">
+            <Box />
+          </div>
+        </div>
+        <div className="broad__item">
+          <div className="broad__item__head">To do</div>
+          <div className="broad__item__content">
+            <Box />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Broadcast;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// import { Provider } from 'react-redux';
+// import store from './store/store.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import './assets/css/style.css'
+import './assets/js/main.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
